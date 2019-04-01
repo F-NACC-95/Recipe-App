9b574fe189e94d2e9a76c1ef5737ce8eb733ae20
Franco Naccarato <f-nacc-21@hotmail.com>
2019-04-01 15:23:56 -0400

    Updates

---
+++ precache-manifest.c7b9745bf756a0373476ffb6c161008d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "83d3eb2e3910b981ab53",
+    "url": "/recipe-app/static/js/runtime~main.03934cd0.js"
+  },
+  {
+    "revision": "ddcc3fb4b7420b8b1ac3",
+    "url": "/recipe-app/static/js/main.c92362c3.chunk.js"
+  },
+  {
+    "revision": "f5e27d4402f279cc4a42",
+    "url": "/recipe-app/static/js/2.75082598.chunk.js"
+  },
+  {
+    "revision": "ddcc3fb4b7420b8b1ac3",
+    "url": "/recipe-app/static/css/main.9e48147f.chunk.css"
+  },
+  {
+    "revision": "a036b9310d073cec9d20a60466ddabd0",
+    "url": "/recipe-app/index.html"
+  }
+];
