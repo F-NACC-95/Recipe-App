11bc31fba509b3c822b8873a8a4c18de170984b5
Franco Naccarato <f-nacc-21@hotmail.com>
2019-04-13 12:16:06 -0400

    switched to hashrouter

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { HashRouter , Switch, Route } from 'react-router-dom';
 
 import App from '../App';
 import RecipePage from './RecipePage';
 
-const Router = () => (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+const HashRouter  = () => (
+    <HashRouter  basename={process.env.PUBLIC_URL}>
         <Switch>
             <Route path={'/'} component={App} exact />
             <Route path={'/recipe/:id'} component={RecipePage} />
         </Switch>
-    </BrowserRouter>
+    </HashRouter >
 )
 
 export default Router;
