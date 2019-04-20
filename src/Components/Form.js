7b23d9a612a46cf413703159ac8cfe5ded2dc400
Franco Naccarato <f-nacc-21@hotmail.com>
2019-04-20 15:44:38 -0400

    search button improvements

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -4,8 +4,8 @@ const Form = (props) => {
     return (
         <div>
             <form onSubmit={props.FetchRecipes} className='form'>
-                <input type='text' name='recipe' placeholder='Search for recipes...'/>
-                <button>Search</button>
+                <input type='text' name='recipe' placeholder='Enter a search term...'/>
+                <button class="form__button">Find Recipes</button>
             </form>
         </div>
     )
